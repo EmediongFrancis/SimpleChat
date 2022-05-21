503892603239dcb78017efc9eaaa1b92f8248704
Emediong Francis <emediongfrancis@gmail.com>
2022-05-21 10:45:53 +0100

    Create server script

--- chatServer.js
+++ chatServer.js
@@ -1,1 +1,5 @@
-var express = require('express');
+import express from 'express';
+var app = express();
+var server = app.listen(3000, () => {
+    console.log('Chat Server is running on port', server.address().port);
+});
