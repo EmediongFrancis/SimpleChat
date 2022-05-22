c3ac53454cdb158ab4a427392fa885f9c7866fb6
Emediong Francis <emediongfrancis@gmail.com>
2022-05-22 21:29:35 +0100

    Complete basic functionality

--- chatServer.js
+++ chatServer.js
@@ -1,69 +1,47 @@
-// Import installed modules.
-var express = require('express');
+// Import and use installed modules.
+const express = require('express');
+const http = require('http');
+
 const app = express();
-var http = require("http");
 const server = http.createServer(app);
-const io = require("socket.io")(server)
-const path = require("path");
 
-app.use(express.static(path.join(__dirname)));
+const io = require('socket.io')(server);
+const path = require('path');
 
+// Configure Express to serve static files in the public folder.
+app.use(express.static(path.join(__dirname, './public')));
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/UserInterface.html");
+// Route for the index page.
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/UserInterface.html'));
 });
 
-var name;
+// Initialie placeholder variable.
+let name;
 
-io.on("connection", (socket) => {
-    console.log("New user connected!");
+// Initialize the socket.io server and monitor for connections.
+io.on('connection', (socket) => {
+  console.log('New user connected!');
 
-    socket.on("user-alert", (username) => {
-        name = username;
-        io.emit("chat-message", `${name} has joined the chat! Please be civil!!`);
-    });
+  // Monitor for the 'name' event.
+  socket.on('join-alert', (username) => {
+    name = username;
+    io.emit('chat-message', `${name} joined the room!`);
+  });
 
-    socket.on("disconnect", () => {
-        console.log("User disconnected!");
-        io.emit("chat-message", `${name} left the chat!`);
-    });
+  // Monitor for disconnects.
+  socket.on('disconnect', () => {
+    console.log('User disconnected!');
+    io.emit('chat-message', `${name} left the room!`);
+  });
 
-    socket.on("chat-message", (msg) => {
-        socket.broadcast.emit("chat-message", msg);
-    });
+  // Broadcast messages to all users.
+  socket.on('chat-message', (msg) => {
+    socket.broadcast.emit('chat-message', msg);
+  });
 });
 
+// Initialize the server.
 server.listen(3000, () => {
-    console.log("Server listening on port 3000!");
+  console.log('Chat server listening on port 3000!');
 });
-
-// // Assign express() function to variable and set port to 3000.
-// app.set("port", process.env.PORT || 3000);
-// app.use(express.static(path.join(__dirname)));
-
-// // Configure the server.
-// let server = http.createServer(app).listen(app.get("port"), () => {
-//     console.log("Server listening on port " + app.get("port"));
-// });
-
-// // Create socket.io server and set nickname for each client.
-// io.sockets.on("connection", (socket) => {
-//     console.log("A new client has connected.");
-//     socket.on("name", (name) => {
-//         socket.set("username", name);
-//     });
-//     // Send message to all clients.
-//     socket.on("message", (message) => {
-//         socket.get("username", (err, name) => {
-//             let username = err ? "Anonymous" : name;
-
-//             let payload = {
-//                 message: message,
-//                 name: username
-//             };
-
-//             socket.emit("message", payload);
-//             socket.broadcast.emit("message", payload);
-//         });
-//     });
-// });
